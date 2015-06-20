30e8bf3353cc3a89561aafa5174a4889b6ce154b
rahulsjain <rahuljainscool@gmail.com>
2015-06-21 00:39:45 +0530

    rahul menu changes user login
    rahul menu changes user login

--- application/views/common/userpages/head_links_new.php
+++ application/views/common/userpages/head_links_new.php
@@ -3,6 +3,7 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/animate.css" />
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/css3-buttons.css" />
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/app.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/style_r.css" />
 <!--<script type="text/javascript" src="<?php echo base_url(); ?>public/js/jquery-1.8.2.min.js"></script>-->
 <script type="text/javascript" src="<?php echo base_url(); ?>public/js/jquery-1.10.1.min.js"></script>  
 <script type="text/javascript" src="<?php echo base_url(); ?>public/js/cssua.min.js"></script>
--- application/views/common/userpages/leftnav_new.php
+++ application/views/common/userpages/leftnav_new.php
@@ -1,12 +1,12 @@
 <?php $userDetails=unserialize($this->session->userdata('fx_user_details')); ?>
 <?php $pamm_count=$this->pamm_manager_model->is_pamm_count($userDetails->id);?>
 <ul class="nav_emt j_left_nav">
-    <li><a href="<?php echo site_url('userpages/homenew'); ?>" class="active">Home</a></li>
-    <li><a href="<?php echo site_url('userpages/depositnew'); ?>">Deposit</a></li>
-    <li><a href="<?php echo site_url('userpages/withdrawalnew'); ?>">Withdrawal</a></li>
-    <li><a href="<?php echo site_url('userpages/mywallet'); ?>"> My Wallet</a></li>     
-    <li><a href="#<?php echo $this->config->item('project_name') ?>_menu" class="collapse j_submenu_toggle">Reports</a></li>
-    <ul class="lft_sub_menu">
+    <li class="menu-item-simple-parent"><a href="<?php echo site_url('userpages/homenew'); ?>" class="active">Home</a></li>
+    <li class="menu-item-simple-parent"><a href="<?php echo site_url('userpages/depositnew'); ?>">Deposit</a></li>
+    <li class="menu-item-simple-parent"><a href="<?php echo site_url('userpages/withdrawalnew'); ?>">Withdrawal</a></li>
+    <li class="menu-item-simple-parent"><a href="<?php echo site_url('userpages/mywallet'); ?>"> My Wallet</a></li>     
+    <li class="menu-item-simple-parent"><a href="#<?php echo $this->config->item('project_name') ?>_menu" class="j_submenu_toggle">Reports</a>
+    <ul class="lft_sub_menu submenu">
         <li>
             <a href="<?php echo site_url('userpages/trading_history'); ?>"> Trading History </a>
         </li>
@@ -19,8 +19,9 @@
             <a href="<?php echo site_url('userpages/deposits_withdrawls'); ?>"> Deposits/Withdrawals </a>
         </li>
 	</ul>
-    <li><a href="#<?php echo $this->config->item('project_name') ?>_menu" class="collapse j_submenu_toggle">My Account</a></li>
-    <ul class="lft_sub_menu">
+    </li>
+    <li class="menu-item-simple-parent"><a href="#<?php echo $this->config->item('project_name') ?>_menu" class="j_submenu_toggle">My Account</a>
+    <ul class="lft_sub_menu submenu">
 <!--        <li>
             <a href="<?php echo site_url('userpages/change_leverage'); ?>"> Change Leverage </a>
         </li>-->
@@ -35,26 +36,26 @@
         </li>
         
         <?php if($userDetails->account_for=='3'){?>
-	<li><a href="<?php echo site_url('userpages/my_program'); ?>"> My PAMM Program</a></li>
-	<li><a href="<?php echo site_url('userpages/pamm_manager'); ?>"> <?php if($pamm_count>0) echo 'Edit'; else echo 'Create';?> PAMM Manager</a></li>
-	<li><a href="<?php echo site_url('userpages/view_investors'); ?>"> View PAMM Investors</a></li>        
+	<li class="menu-item-simple-parent"><a href="<?php echo site_url('userpages/my_program'); ?>"> My PAMM Program</a></li>
+	<li class="menu-item-simple-parent"><a href="<?php echo site_url('userpages/pamm_manager'); ?>"> <?php if($pamm_count>0) echo 'Edit'; else echo 'Create';?> PAMM Manager</a></li>
+	<li class="menu-item-simple-parent"><a href="<?php echo site_url('userpages/view_investors'); ?>"> View PAMM Investors</a></li>        
 	<?php }?>
         <?php  if($userDetails->account_for=='2'){?>
-	<li><a href="<?php echo site_url('userpages/get_managers'); ?>"> PAMM Managers</a></li>
-	<li><a href="<?php echo site_url('userpages/my_fundprogram'); ?>"> My PAMM Funds</a></li>        
+	<li class="menu-item-simple-parent"><a href="<?php echo site_url('userpages/get_managers'); ?>"> PAMM Managers</a></li>
+	<li class="menu-item-simple-parent"><a href="<?php echo site_url('userpages/my_fundprogram'); ?>"> My PAMM Funds</a></li>        
 	<?php }?>
 
-    </ul>
-    <li><a href="#<?php echo $this->config->item('project_name') ?>_menu" class="collapse j_submenu_toggle">Upload Documents</a></li>
-    <ul class="lft_sub_menu">
+    </ul></li>
+    <li class="menu-item-simple-parent"><a href="#<?php echo $this->config->item('project_name') ?>_menu" class="j_submenu_toggle">Upload Documents</a>
+    <ul class="lft_sub_menu submenu">
         <li>
             <a href="<?php echo site_url('userpages/validation_documents'); ?>"> Validation Documents </a>
         </li>
         <li>
             <a href="<?php echo site_url('userpages/additional_documents'); ?>"> Additional Documents </a>
         </li>
-    </ul>
+    </ul></li>
     <!--<li><a href="<?php //echo site_url('userpages/trading_signals'); ?>">Trading Signals</a></li>-->
-    <li><a href="<?php echo site_url('userpages/support_request'); ?>">Support Request</a></li>
+    <li class="menu-item-simple-parent"><a href="<?php echo site_url('userpages/support_request'); ?>">Support Request</a></li>
 
 </ul>
